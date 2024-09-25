3b4355df428ffb57365ba87216fb5cb42aef82f1
mrkgbr <116895777+mrkgbr@users.noreply.github.com>
2024-09-25 10:53:28 +0200

    Add tests

--- iteration/iteration_test.go
+++ iteration/iteration_test.go
@@ -1,18 +1,45 @@
 package iteration
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestRepeat(t *testing.T) {
-	repeated := Repeat("a")
-	expected := "aaaaa"
+	t.Run("repeat 'a' 5 times", func(t *testing.T) {
+		repeated := Repeat("a", 5)
+		expected := "aaaaa"
 
-	if repeated != expected {
-		t.Errorf("expected %q but got %q", expected, repeated)
-	}
+		if repeated != expected {
+			t.Errorf("expected %q but got %q", expected, repeated)
+		}
+	})
+	t.Run("repeat 'a' 0 times", func(t *testing.T) {
+		repeated := Repeat("a", 0)
+		expected := ""
+
+		if repeated != expected {
+			t.Errorf("expected %q but got %q", expected, repeated)
+		}
+	})
+	t.Run("repeat 'a' -1 times", func(t *testing.T) {
+		repeated := Repeat("a", -1)
+		expected := ""
+
+		if repeated != expected {
+			t.Errorf("expected %q but got %q", expected, repeated)
+		}
+	})
 }
 
 func BenchmarkRepeat(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		Repeat("a")
+		Repeat("a", 5)
 	}
+}
+
+func ExampleRepeat() {
+	repeat := Repeat("a", 6)
+	fmt.Println(repeat)
+	// Output: aaaaaa
 }
