6acba969411c34ba9bf0f7b234f6f9863a31b7e8
mrkgbr <116895777+mrkgbr@users.noreply.github.com>
2024-09-25 11:29:21 +0200

    Add comment doc

--- iteration/iteration.go
+++ iteration/iteration.go
@@ -1,5 +1,7 @@
 package iteration
 
+// Returns a new string consisting of the input character
+// repeated 'repeatCount' times.
 func Repeat(character string, repeatCount int) string {
 	var repeated string
 	for i := 0; i < repeatCount; i++ {
